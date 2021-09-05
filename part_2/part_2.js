5f2750e073ea34ecb5b6404cd09b033d60b5d504
BasharAlZawahreh <zawahrehbashar@gmail.com>
2021-09-05 20:53:16 +0300

    P2

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -5,32 +5,46 @@ PART 3 [General Information]:
 Type the correct answer below each question:
 
 Q1) Why do we use databases?
-
+To Save Data and use it again and again.
+also to render it to all users.
 
 Q2) What are JWTs used for?.
-
+json web token, whit serves us in having token.
 
 Q3) What are the main three sections of a JWT?
-
+header
+payload
+signeture
 
 Q4) Name three types of express middle-wares.
-
+built in
+app 
+third party
 
 Q5) What is the `effect` hook used for in react?
-``
+to have the side effect
 
 Q6) What is the `state` hook used for in react?
-
+to make variable statefull and  render it in every change it has
 
 Q7) List three thing you can use to create a responsive web application.
+useEffect 
+useState
+useContext
 
 
 Q8) Name two of mongoose middle-wares and explain what they are used for.
 
 
-Q9) What is the difference between authentication and authorization?
 
+Q9) What is the difference between authentication and authorization?
+Authentication:
+To know if the user has an access to database, without considering what he can do.
+***Who are you***
+
+Authorization:
+To know what could user do and see.
+***What can you do***
 
 Q10) Why do we save some information as an environment variable (in .env file)?
-
-*/
+to save it as secrets and not to allow any one to access them*/
